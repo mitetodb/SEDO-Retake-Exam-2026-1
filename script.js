9ba9dbabc2c24cc011be1bf7bf0aec174a908501
Diyan Apostolov <6802398+DiyanApostolov@users.noreply.github.com>
2026-03-18 08:40:20 +0200

    Initial commit

---
+++ script.js
@@ -0,0 +1,4 @@
+document.getElementById('apply-btn').addEventListener('click', function () {
+  var c = document.getElementById('color-input').value;
+  document.getElementById('box').style.backgroundColor = c;
+});
